d424a2e689a3d7fea6a8d1f0c94b8a8a89daa803
Dalvs <lopesribeirodalvana20@gmail.com>
2025-09-25 19:17:54 -0300

    add

---
+++ react/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import CardJogador from "./components/CardJogador";
+import Fallen from "../src/assets/fallen.jpg"
+
+// Função proincipal do App 
+export default function App(){
+    // Array de objetos respresentando os jogadores
+    // Cada objeto tem id e as props nome, posicao, time, avaliacao e imagem
+    const jogadores = [
+        {
+            id: 1,
+            nome: "Fallen",
+            posicao: "IGL",
+            time: "Fúria",
+            avaliacao: 10,
+            imagem: 
+        }
+    ]
+}
