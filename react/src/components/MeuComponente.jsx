a4690be1d3060a3c1ffa6a9ec5436f6b21e69e61
Dalvs <lopesribeirodalvana20@gmail.com>
2025-09-25 21:10:10 -0300

    add

--- react/src/components/MeuComponente.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-const MeuComponente = () =>{
-    return(
-        <div className="container">
-            <h1> Olá, Mundo!</h1>
-        </div>
-    )
-}
-export default MeuComponente
